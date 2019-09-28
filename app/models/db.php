550e6bca3d1f1dab83a4ddd7de744fd6be4b490e
Vaibhav <vrongmeal@gmail.com>
2019-09-28 14:39:53 +0530

    Update database
    Signed-off-by: Vaibhav <vrongmeal@gmail.com>

--- app/models/db.php
+++ app/models/db.php
@@ -5,7 +5,7 @@ class DB {
 
     public static function get_instance() {
         if (!self::$instance) {
-            self::$instance = new PDO("mysql:host=51.158.118.84;port=33060;dbname=readit", "vrongmeal", "password");
+            self::$instance = new PDO("mysql:host=51.158.118.84;port=33060;dbname=readit2", "vrongmeal", "password");
             self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         return self::$instance;
